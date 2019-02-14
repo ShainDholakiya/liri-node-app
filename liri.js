2397bacbf983533501ac581436b64aa9fd30a03a
Shain Dholakiya <shainshain99@gmail.com>
2019-02-13 23:51:05 -0500

    initial site files

--- liri.js
+++ liri.js
@@ -7,20 +7,21 @@ var moment = require("moment");
 var fs = require('fs');
 var command = process.argv[2];
 
-if (command === "concert-this") {
-  findConcert();
-}
-else if (command === "spotify-this-song") {
-  findSpotify();
-}
-else if (command === "movie-this") {
-  findMovie();
-}
-else if (command === "do-what-it-says") {
-  findRandom();
-}
-else {
-  console.log("Command not found");
+switch (command) {
+  case "concert-this":
+    findConcert();
+    break;
+  case "spotify-this-song":
+    findSpotify();
+    break;
+  case "movie-this":
+    findMovie();
+    break;
+  case "do-what-it-says":
+    findRandom();
+    break;
+  default:
+    console.log("Command not found");
 }
 
 function findConcert() {
@@ -39,7 +40,7 @@ function findConcert() {
             response.data[i].venue.country);
         }
         console.log("Date of the Event: " + moment(response.data[i].datetime).format("MM/DD/YYYY"));
-        console.log("--------------------------------------------");
+        line();
       }
     });
 }
@@ -53,12 +54,12 @@ function findSpotify() {
       query: "The Sign",
       limit: 10
     }).then(function (response) {
-      console.log("--------------------------------------------");
+      line();
       console.log("Artist(s): " + response.tracks.items[9].album.artists[0].name);
       console.log("Track: " + response.tracks.items[9].name);
       console.log("Preview Song link: " + response.tracks.items[9].preview_url);
       console.log("Album: " + response.tracks.items[9].album.name);
-      console.log("--------------------------------------------");
+      line();
     });
   }
   else {
@@ -67,12 +68,12 @@ function findSpotify() {
       query: songName,
       limit: 1
     }).then(function (response) {
-      console.log("--------------------------------------------");
+      line();
       console.log("Artist(s): " + response.tracks.items[0].album.artists[0].name);
       console.log("Track: " + response.tracks.items[0].name);
       console.log("Preview Song link: " + response.tracks.items[0].preview_url);
       console.log("Album: " + response.tracks.items[0].album.name);
-      console.log("--------------------------------------------");
+      line();
     });
   }
 }
@@ -84,7 +85,7 @@ function findMovie() {
     axios
       .get("http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy")
       .then(function (response) {
-        console.log("--------------------------------------------");
+        line();
         console.log("Title: " + response.data.Title);
         console.log("Year Released: " + response.data.Year);
         console.log("IMDB Rating: " + response.data.imdbRating);
@@ -93,7 +94,7 @@ function findMovie() {
         console.log("Language(s): " + response.data.Language);
         console.log("Plot: " + response.data.Plot);
         console.log("Actors: " + response.data.Actors);
-        console.log("--------------------------------------------");
+        line();
       }
       );
   }
@@ -101,7 +102,7 @@ function findMovie() {
     axios
       .get("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy")
       .then(function (response) {
-        console.log("--------------------------------------------");
+        line();
         console.log("Title: " + response.data.Title);
         console.log("Year Released: " + response.data.Year);
         console.log("IMDB Rating: " + response.data.imdbRating);
@@ -110,14 +111,14 @@ function findMovie() {
         console.log("Language(s): " + response.data.Language);
         console.log("Plot: " + response.data.Plot);
         console.log("Actors: " + response.data.Actors);
-        console.log("--------------------------------------------");
+        line();
       }
       );
   }
 }
 
 function findRandom() {
-  fs.readFile("./random.txt", "utf-8", function(err, response) {
+  fs.readFile("./random.txt", "utf-8", function (err, response) {
     let splitData = response.split(",");
     var command = splitData[0];
     var info = splitData[1];
@@ -128,49 +129,53 @@ function findRandom() {
         query: info,
         limit: 1
       }).then(function (response) {
-        console.log("--------------------------------------------");
+        line();
         console.log("Artist(s): " + response.tracks.items[0].album.artists[0].name);
         console.log("Track: " + response.tracks.items[0].name);
         console.log("Preview Song link: " + response.tracks.items[0].preview_url);
         console.log("Album: " + response.tracks.items[0].album.name);
-        console.log("--------------------------------------------");
+        line();
       });
     }
     else if (command === "movie-this") {
       axios
-      .get("http://www.omdbapi.com/?t=" + info + "&y=&plot=short&apikey=trilogy")
-      .then(function (response) {
-        console.log("--------------------------------------------");
-        console.log("Title: " + response.data.Title);
-        console.log("Year Released: " + response.data.Year);
-        console.log("IMDB Rating: " + response.data.imdbRating);
-        console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
-        console.log("Country/Countries Produced: " + response.data.Country);
-        console.log("Language(s): " + response.data.Language);
-        console.log("Plot: " + response.data.Plot);
-        console.log("Actors: " + response.data.Actors);
-        console.log("--------------------------------------------");
-      }
-      );
+        .get("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy")
+        .then(function (response) {
+          line();
+          console.log("Title: " + response.data.Title);
+          console.log("Year Released: " + response.data.Year);
+          console.log("IMDB Rating: " + response.data.imdbRating);
+          console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+          console.log("Country/Countries Produced: " + response.data.Country);
+          console.log("Language(s): " + response.data.Language);
+          console.log("Plot: " + response.data.Plot);
+          console.log("Actors: " + response.data.Actors);
+          line();
+        }
+        );
     }
     else if (command === "concert-this") {
       axios
-      .get("https://rest.bandsintown.com/artists/" + info + "/events?app_id=codingbootcamp")
-      .then(function (response) {
-        for (var i = 0; i < response.data.length; i++) {
-          console.log("Name of the venue: " + response.data[i].venue.name);
-          if (response.data[i].venue.region !== "") {
-            console.log("Venue location: " + response.data[i].venue.city + ", " +
-              response.data[i].venue.region + ", " + response.data[i].venue.country);
+        .get("https://rest.bandsintown.com/artists/" + info + "/events?app_id=codingbootcamp")
+        .then(function (response) {
+          for (var i = 0; i < response.data.length; i++) {
+            console.log("Name of the venue: " + response.data[i].venue.name);
+            if (response.data[i].venue.region !== "") {
+              console.log("Venue location: " + response.data[i].venue.city + ", " +
+                response.data[i].venue.region + ", " + response.data[i].venue.country);
+            }
+            else {
+              console.log("Venue location: " + response.data[i].venue.city + ", " +
+                response.data[i].venue.country);
+            }
+            console.log("Date of the Event: " + moment(response.data[i].datetime).format("MM/DD/YYYY"));
+            line();
           }
-          else {
-            console.log("Venue location: " + response.data[i].venue.city + ", " +
-              response.data[i].venue.country);
-          }
-          console.log("Date of the Event: " + moment(response.data[i].datetime).format("MM/DD/YYYY"));
-          console.log("--------------------------------------------");
-        }
-      });
+        });
     }
   });
+}
+
+function line() {
+  console.log("--------------------------------------------");
 }
